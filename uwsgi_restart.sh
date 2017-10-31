abf7d44c6d4de2c0fbce4e32b506ebfa479b5350
light-white <light-white@outlook.com>
2017-10-31 15:40:59 +0800

    change restart.sh

--- uwsgi_restart.sh
+++ uwsgi_restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 psid=`ps aux | grep "meeting_uwsgi" | grep -v "grep" | wc -l`
-if [ $psid -gt 4 ]
+if [ ${psid} -gt 4 ]
 then
     echo "uwsgi is running!"
     killall -9 uwsgi
@@ -8,5 +8,5 @@ then
 else
     echo "No uwsgi running!"
 fi
-/usr/bin/uwsgi /home/django/meeting/meeting_uwsgi.ini
+uwsgi /home/django/meeting/meeting_uwsgi.ini
 echo "Start uwsgi service [OK]"
