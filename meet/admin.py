5d4716370906215536ca392a26e59fe0481365e8
light-white <light-white@outlook.com>
2018-01-02 21:15:49 +0800

    增加参会人员查看

--- meet/admin.py
+++ meet/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from meet.models import Meet
+from meet.models import Meetmember
 
 # Register your models here.
 
 admin.site.register(Meet)
+admin.site.register(Meetmember)
